e12ad744da85ad52b7414e7b2be56e4b38fda955
Andrea Piacentini <piacentini.palm@gmail.com>
2021-12-01 11:11:55 +0100

    First trial of the saber and oops update. All the tests work BUT both the localized Dirac tests (3d and 4d) where the field_to_atlas interface fails (can't find a variable name) Please, Ben, we need your help This commit refers to issue #9

--- src/mains/aqVar.cc
+++ src/mains/aqVar.cc
@@ -8,17 +8,15 @@
 #include "aq/instantiateAqChangeVarFactory.h"
 #include "aq/Traits.h"
 #include "oops/runs/Run.h"
-#include "saber/oops/instantiateCovarFactory.h"
-#include "saber/oops/instantiateVariableChangeFactory.h"
-
 #include "oops/runs/Variational.h"
+#include "saber/oops/instantiateCovarFactory.h"
+#include "saber/oops/instantiateLocalizationFactory.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
   aq::instantiateAqChangeVarFactory();
   saber::instantiateCovarFactory<aq::Traits>();
   saber::instantiateLocalizationFactory<aq::Traits>();
-  saber::instantiateVariableChangeFactory<aq::Traits>();
   oops::Variational<aq::Traits, aq::ObsTraits> var;
   return run.execute(var);
 }
